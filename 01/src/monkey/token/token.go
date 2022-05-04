05733dcc221487913b56a9f0072a600d903959a0
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-05-04 17:45:58 +0900

    [change] 書籍の位置までロールバック

--- 01/src/monkey/token/token.go
+++ 01/src/monkey/token/token.go
@@ -2,29 +2,24 @@ package token
 
 type TokenType string
 
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
-	// Identifiers + literals
+	// 識別子 + リテラル
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"   // 1343456
 
-	// Operators
-	ASSIGN   = "="
-	PLUS     = "+"
-	MINUS    = "-"
-	BANG     = "!"
-	ASTERISK = "*"
-	SLASH    = "/"
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
 
-	LT = "<"
-	GT = ">"
-
-	EQ     = "=="
-	NOT_EQ = "!="
-
-	// Delimiters
+	// デリミタ
 	COMMA     = ","
 	SEMICOLON = ";"
 
@@ -33,34 +28,7 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
-	// Keywords
+	// キーワード
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
-	TRUE     = "TRUE"
-	FALSE    = "FALSE"
-	IF       = "IF"
-	ELSE     = "ELSE"
-	RETURN   = "RETURN"
 )
-
-type Token struct {
-	Type    TokenType
-	Literal string
-}
-
-var keywords = map[string]TokenType{
-	"fn":     FUNCTION,
-	"let":    LET,
-	"true":   TRUE,
-	"false":  FALSE,
-	"if":     IF,
-	"else":   ELSE,
-	"return": RETURN,
-}
-
-func LookupIdent(ident string) TokenType {
-	if tok, ok := keywords[ident]; ok {
-		return tok
-	}
-	return IDENT
-}
