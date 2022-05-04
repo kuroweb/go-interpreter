7dfc45cd20d96a8e921949327603a8978d0c65dd
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-05-04 19:18:02 +0900

    [change] 01

--- 01/src/monkey/lexer/lexer.go
+++ 01/src/monkey/lexer/lexer.go
@@ -4,9 +4,9 @@ import "github.com/kuromitsu0104/go-interpreter/token"
 
 type Lexer struct {
 	input        string
-	position     int
-	readPosition int
-	ch           byte
+	position     int    // 入力における現在の位置(現在の文字を指し示す)
+	readPosition int		// これから読み込む位置(現在の文字の次)
+	ch           byte   // 現在検査中の文字
 }
 
 func New(input string) *Lexer {
@@ -48,6 +48,13 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -56,4 +63,16 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func(l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch >= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
