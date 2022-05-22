0df6cd4f08d689b297e286aadc0ae39a5b9b625b
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-05-22 16:42:53 +0900

    [change] トレース用の関数をコメントアウト

--- monkey/02/parser/parser.go
+++ monkey/02/parser/parser.go
@@ -179,6 +179,8 @@ func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 
 // ExpresstionStatement ASTノードを生成
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	// defer untrace(trace("parseExpressionStatement"))
+
 	stmt := &ast.ExpressionStatement{Token: p.curToken}
 
 	stmt.Expression = p.parseExpression(LOWEST)
@@ -191,6 +193,8 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 }
 
 func (p *Parser) parseExpression(precedence int) ast.Expression {
+	// defer untrace(trace("parseExpression"))
+
 	prefix := p.prefixParseFns[p.curToken.Type] // prefixのトークンタイプに対応するparser関数を取得
 	if prefix == nil {
 		p.noPrefixParseFnError(p.curToken.Type)
@@ -217,6 +221,8 @@ func (p *Parser) parseIdentifier() ast.Expression {
 }
 
 func(p *Parser) parseIntegerLiteral() ast.Expression {
+	// defer untrace(trace("parseIntegerLiteral"))
+
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
@@ -234,6 +240,8 @@ func(p *Parser) parseIntegerLiteral() ast.Expression {
 // token.BANG, token.MINUSのときに呼ばれるメソッド
 // 次のトークンを読み込みんで、expression.RightにASTノードを格納する
 func(p *Parser) parsePrefixExpression() ast.Expression {
+	// defer untrace(trace("parsePrefixExpression"))
+
 	expression := &ast.PrefixExpression{
 		Token: p.curToken,
 		Operator: p.curToken.Literal,
@@ -286,6 +294,8 @@ func (p *Parser) curPrecedence() int {
 // 	Right: 右側のASTノード,
 // }
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	// defer untrace(trace("parseInfixExpression"))
+
 	expression := &ast.InfixExpression{
 		Token: p.curToken,
 		Operator: p.curToken.Literal,
