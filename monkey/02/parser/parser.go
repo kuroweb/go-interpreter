88de2911c7df0b6fd7994102eb729fd96da2f7d3
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-07-31 16:13:07 +0900

    chap2.8

--- monkey/02/parser/parser.go
+++ monkey/02/parser/parser.go
@@ -121,8 +121,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// 解析中のトークンがセミコロンになるまでポインタを進める
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -157,8 +160,9 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
-	for !p.curTokenIs(token.SEMICOLON) {
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
