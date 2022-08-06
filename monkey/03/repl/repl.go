535ff54ff31f3a00d22216e79616e71e5bab6a30
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-08-06 17:30:45 +0900

    chap3-5-2

--- monkey/03/repl/repl.go
+++ monkey/03/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/kuromitsu0104/go-interpreter/monkey/03/evaluator"
 	"github.com/kuromitsu0104/go-interpreter/monkey/03/lexer"
 	"github.com/kuromitsu0104/go-interpreter/monkey/03/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
